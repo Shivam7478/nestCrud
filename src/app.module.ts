8a9f508307673a4fa31256a6847dce00eed4fe82
AkshadaD <akshata2696@gmail.com>
2020-07-23 14:27:11 +0530

    Feat added file uploading feature

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,11 @@ import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { MulterModule } from '@nestjs/platform-express';
 @Module({
-  imports: [ProductsModule,MongooseModule.forRoot('mongodb+srv://admin:admin@reactlogin-roglq.mongodb.net/test?retryWrites=true&w=majority'), AuthModule, UsersModule],
+  imports: [MulterModule.register({
+    dest: './files',
+  }),ProductsModule,MongooseModule.forRoot('mongodb+srv://admin:admin@reactlogin-roglq.mongodb.net/test?retryWrites=true&w=majority'), AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
-
+import { Controller, Post, UseInterceptors, UploadedFile } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { editFileName, imageFileFilter } from 'src/utils/file-uploading.utils';
+import {diskStorage} from 'multer';
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor() {}
+   
+    @Post('uploadImage')
+    @UseInterceptors(
+        FileInterceptor('profileImage',
+        {
+            storage: diskStorage({
+              destination: './files',
+              filename: editFileName,
+            }),
+            fileFilter: imageFileFilter,
+          })
+      )
+    async uploadImage(@UploadedFile() file
+    ) {
+        const response = {
+            originalname: file.originalname,
+            filename: file.filename,
+          };
+      return response;
+    }
+}
